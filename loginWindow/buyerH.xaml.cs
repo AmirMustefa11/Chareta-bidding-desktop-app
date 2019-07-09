9c10e68ff7fa287bb7aed4a16971bf20c961efed
Amir Mustefa <amirmustefa707@gmail.com>
2019-07-09 03:32:30 +0300

    bidding app last commit

---
+++ loginWindow/buyerH.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace loginWindow
+{
+    /// <summary>
+    /// Interaction logic for buyerH.xaml
+    /// </summary>
+    public partial class buyerH : Window
+    {
+        public buyerH()
+        {
+            InitializeComponent();
+        }
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+
+            switch (index)
+            {
+                case 0:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new UserControlEscolha());
+                    break;
+                case 1:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new UserControlInicio ());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+       
+    }
+}
---
+++ loginWindow/registration.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace loginWindow
+{
+    /// <summary>
+    /// Interaction logic for registration.xaml
+    /// </summary>
+    public partial class registration : Window
+    {
+        public registration()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+   
+        private void signup_btn_clicked(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow m = new MainWindow();
+            this.Close();
+            m.Show();
+        }
+    }
+}
